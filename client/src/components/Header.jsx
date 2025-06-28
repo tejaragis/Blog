8fad20cb155e4145cdf2a5e906e1c89f3c6e4de3
tejaragis <teja900069@gmail.com>
2025-06-28 23:28:04 +0530

    git ignore add

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import {Navbar, TextInput,Button} from 'flowbite-react'
-import {Link} from "react-router-dom";
+import {Link,useLocation} from "react-router-dom";
 import {AiOutlineSearch} from 'react-icons/ai';
+import { IoMoonSharp } from "react-icons/io5";
+import { IoSunnySharp } from "react-icons/io5";
+
 export default function Header() {
+  const path=useLocation().pathname;
+  
   return (
    <Navbar className='border-b-2'> 
         <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -16,6 +21,34 @@ export default function Header() {
         <Button className='w-12 h-10 lg:hidden' color='gray' pill >
           <AiOutlineSearch/>
         </Button>
+        <div className='flex gap-2 md:order-2'>
+          
+          <Button pill className='w-12 h-10 '>
+                        <IoSunnySharp />
+          </Button>
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue'  color='gray' pill outline>Sign In</Button>
+          </Link>
+          <Navbar.Toggle/>
+
+        </div>
+        <Navbar.Collapse>
+            <Navbar.Link active={path ==='/'} as={'div'}>
+              <Link to="/" >
+                   Home
+              </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path ==='/about'} as={'div'}>
+              <Link to="/about">
+                   About
+              </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path ==='/projects'} as={'div'}>
+              <Link to="/projects">
+                   Porjects
+              </Link>
+            </Navbar.Link>
+          </Navbar.Collapse>
     </Navbar>
   
   )
