056043adea5e03cd64170fc73c1426c1217ff060
tejaragis <teja900069@gmail.com>
2025-08-04 23:29:08 +0530

    Error handler

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userrouter from './routes/user.route.js';
+import authrouter from './routes/auth.route.js';
 const app=express();
+app.use(express.json());
 dotenv.config();
 mongoose.connect(process.env.Mongo)
          .then(()=>{console.log('Mongodb is connected')})
@@ -12,3 +15,5 @@ mongoose.connect(process.env.Mongo)
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
 });
+app.use('/api/user',userrouter)
+app.use('/api/auth',authrouter)
